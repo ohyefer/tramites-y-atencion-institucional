c079bcc3d902ea510dd94a1fe9c07aa891ddb748
Yeferson Bravo ツ <40337937+ohyefer@users.noreply.github.com>
2026-05-11 06:02:03 -0400

    Add files via upload

--- admin/ajax/buscar_fechaV.php
+++ admin/ajax/buscar_fechaV.php
@@ -1,1 +1,84 @@
+<?php
 
+include("../config/conexion.php");
+$conexion=conectarse();
+$date1 = date("Y-m-d", strtotime($_POST['date1']));
+$date2 = date("Y-m-d", strtotime($_POST['date2']));
+
+if (!empty($_POST['date1']) and  !empty($_POST['date1'])){
+	list($dia,$mes,$anio)=explode("/",$_POST['date1']);
+	$date1="$anio-$mes-$dia";
+
+	
+	list($dia,$mes,$anio)=explode("/",$_POST['date2']);
+	$date2="$anio-$mes-$dia";
+	
+	$sWhere="WHERE `fecha` BETWEEN '$date1' AND '$date2'";
+	
+} else {
+	$sWhere="";	
+}
+
+
+
+$total   = mysqli_query($conexion, "SELECT count(*) FROM publico  $sWhere")or die("Problemas en el select".mysqli_error($conexion));
+
+while ($row = mysqli_fetch_array($total)) {
+			echo 
+
+
+			'
+			Visitantes Registrado por Fecha:&nbsp;'.$row['count(*)'].'';
+}
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+$q_book = mysqli_query($conexion, "SELECT * FROM `publico` $sWhere ORDER BY `fecha` DESC") or die("Problemas en el select:". mysqli_error($conexion));
+
+
+
+
+echo '<table class="table table-striped table-condensed table-hover">
+        	<tr>
+							<th>Cédula</th>
+							<th>Nombre</th>
+							<th>Apellido</th>
+							<th style = "width:20%;">Fecha de Registro</th>
+							
+            
+     
+
+			</tr>';
+
+
+
+$v_book = $q_book->num_rows;
+if($v_book > 0){
+	while($f_book = $q_book->fetch_array()){
+	
+	echo '<tr>
+		<td>'.$f_book['cedula'].'</td>
+		<td>'.$f_book['nombre'].'</td>
+		<td>'.$f_book['apellido'].'</td>
+		<td>'.date("d/m/Y", strtotime($f_book['fecha'])).'</td>
+
+
+	</tr>'
+
+	?>
+	<?php
+}
+}
+else{
+	echo '<tr>
+				<td colspan="6">No se encontraron resultados</td>
+			</tr>';
+}
+echo '</table>';
+
+echo '<br>';
+echo '<br>';
+
+?>
---
+++ admin/ajax/buscar_u.php
@@ -0,0 +1,66 @@
+<?php
+	require_once ("../config/conexion.php");
+?>
+
+<?php
+
+$conexion=conectarse();			
+$action='ajax';
+	if($action == 'ajax'){
+         $q = mysqli_real_escape_string($conexion,(strip_tags($_REQUEST['q'], ENT_QUOTES)));
+		  $sTable = "publico";
+		 $sWhere = "";
+		 $sWhere.="WHERE id_publico<=id_publico";
+		if ( $_GET['q'] != "" )
+		{
+		$sWhere.= " and cedula like '%$q%'";
+			
+		}
+		
+		$sWhere.=" order by cedula asc";
+		
+		$page = (isset($_REQUEST['page']) && !empty($_REQUEST['page']))?$_REQUEST['page']:1;
+
+
+		$registro = mysqli_query($conexion,"SELECT * FROM $sTable $sWhere") or
+    die("Problemas en el select:".mysqli_error($conexion));
+		
+		?>
+
+				<table class="table table-hover" style="border-color: #337ab7;">
+
+					<tr style="color: #fff; background-color: #337ab7; border-color: #337ab7;">
+					
+        
+ 
+          <th>#</th>  
+          <th>Nombre Completo</th>
+          <th>Cédula</th>
+
+
+        <?php $n=1; while ($reg=mysqli_fetch_array($registro)) :?>
+        
+
+         <tr>
+          <td> <?php echo $n++; ?> </td>
+          <td><a href="registrarP.php?cedula=<?php echo $reg['cedula']; ?>
+          "> <?php echo ($reg['nombre'])."&nbsp".($reg['apellido']);?></td></a>
+          <td> <?php echo ($reg['cedula']);?></td>
+        
+                 
+        </tr>
+    </tr>
+
+
+
+
+<?php endwhile; ?>
+
+        </table>
+<?php
+}
+?>
+
+
+
+		
